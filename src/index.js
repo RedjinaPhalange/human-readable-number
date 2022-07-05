5560fd2d373ea35bf22d818207e9b47a4a6fe1a1
ReddjinaPhalange <redjinaphalange@gmail.com>
2022-07-05 14:04:27 +0300

    fix: update function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 module.exports = function toReadable (number) {
     let num = number.toString();
-    const units = 'One Two Three Four Five Six Seven Eight Nine'.split(' ');
-    const teens = 'Ten Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen'.split(' ');
-    const dozens = 'Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety'.split(' ');
+    const units = 'one two three four five six seven eight nine'.split(' ');
+    const teens = 'ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen'.split(' ');
+    const dozens = 'twenty thirty forty fifty sixty seventy eighty ninety'.split(' ');
     if (number == 0){
-        return 'Zero';
+        return 'zero';
     }
     if (number < 10){
         return units[number - 1];
@@ -19,15 +19,21 @@ module.exports = function toReadable (number) {
         return dozens[Math.trunc(number / 10) - 2] + ' ' + units[num[1] - 1];
     }
     if ((number % 100 != 0) && num[1] == 0) {
-        return units[Math.trunc(number / 100) - 1] + ' Hundred' + ' ' + units[num[2] -1];
+        return units[Math.trunc(number / 100) - 1] + ' hundred' + ' ' + units[num[2] -1];
     }
         if ((number % 100 != 0) && num[1] == 1) {
-        return units[Math.trunc(number / 100) - 1] + ' Hundred' + ' ' + teens[num[2]];
+        return units[Math.trunc(number / 100) - 1] + ' hundred' + ' ' + teens[num[2]];
     }
     if ((number % 100 == 0) && number < 1000){
-        return units[Math.trunc(number / 100) - 1] + ' Hundred';
+        return units[Math.trunc(number / 100) - 1] + ' hundred';
     }
-    if ((number % 100 != 0) && number > 100){
-        return units[Math.trunc(number / 100) - 1] + ' Hundred' + ' ' + dozens[num[1] -2] + ' ' + units[num[2] -1];
+            if (num[2] == 0){
+        return units[Math.trunc(number / 100) - 1] + ' hundred' + ' ' + dozens[num[1] -2];
+    }
+    if (number % 100 != 0){
+        return units[Math.trunc(number / 100) - 1] + ' hundred' + ' ' + dozens[num[1] -2] + ' ' + units[num[2] -1];
+    }
+        if (num[2] == 0){
+        return units[Math.trunc(number / 100) - 1] + ' hundred' + ' ' + dozens[num[1] -2];
     }
 }
